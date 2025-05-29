80866c9fe426d74c555afd2e4c205fe164afa764
BlitzDev <145472107+Reese1243@users.noreply.github.com>
2025-05-29 13:11:54 -0600

    finished the Word class for Develop03 project.

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        List<string> test = new List<string>();
+        test = ["this", "is", "a", "list"];
+        Word exampleinstant = new Word();
+        string randomWord = exampleinstant.GetRandomWord(test);
+        Console.WriteLine(randomWord);
+        Console.WriteLine(exampleinstant.CensorWord(randomWord));
+
     }
 }
---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,46 @@
+using System.Security.Cryptography;
+
+class Word
+{
+    string _word;
+    bool _isHidden;
+
+    //gets a random word from a list of strings
+    public string GetRandomWord(List<string> wordList)
+    {
+        Random randomGen = new Random();
+        int wordIndex = randomGen.Next(0, wordList.Count());
+        return wordList[wordIndex];
+    }
+
+
+    // censors a random word with underscores
+    public string CensorWord(string word)
+    {
+        string censorWord = "";
+        int wordLength = word.Length;
+
+        for (int i = 0; i < wordLength; i++)
+        {
+            censorWord = censorWord + "_";
+        }
+
+        return censorWord;
+    }
+
+    // sets the _word attribute to the string argument
+    public void SetWord(string word)
+    {
+        _word = word;
+        _isHidden = true;
+    }
+
+    // displays the _word attribute
+    public void DisplayWord()
+    {
+        Console.WriteLine(_word);
+    }
+
+
+
+}
