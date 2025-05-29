861145cd04b396bf54d684c049e6ee68fb766719
BlitzDev <145472107+Reese1243@users.noreply.github.com>
2025-05-29 13:10:37 -0600

    created a custom project called WordCOunter.

---
+++ WordCounter/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    public static void Main(string[] args)
+    {
+        WordCounter wordCounter = new WordCounter("This is a string of text");
+        wordCounter.DisplayWords();
+
+        int count = wordCounter.CountSingleWord("text");
+        Console.WriteLine(count);
+
+    }
+}
