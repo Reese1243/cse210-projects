15c0b03194c4d933c9b044c081a2707ae8813243
BlitzDev <145472107+Reese1243@users.noreply.github.com>
2025-06-03 10:00:33 -0600

    added policeman class

---
+++ Person/Policeman.cs
@@ -0,0 +1,12 @@
+class PoliceMan : Person
+{
+    private string _weapons;
+    public PoliceMan(string firstName, string lastName, int age, string weapons) :
+    base(firstName, lastName, age)
+    {
+        _weapons = _weapons;
+    }
+
+
+    
+}
--- Person/Program.cs
+++ Person/Program.cs
@@ -2,6 +2,11 @@
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("test");
+        Person myPerson = new Person("jim", "Bob", 20);
+
+        Console.WriteLine(myPerson.GetPersonInformation());
+
+        PoliceMan myPoliceman = new PoliceMan("cooper", "silver", 34, "Baton");
+        Console.WriteLine(myPoliceman.GetPersonInformation());
     }
 }
